86127f2ffe899c219fbf1298e1688f76b74215fa
Jung <maestro214>
2023-02-23 23:55:43 +0900

    gradle 추가 위치권한 요청

--- app/src/main/java/com/project/misemon/MainActivity.kt
+++ app/src/main/java/com/project/misemon/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.project.misemon
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import java.util.jar.Manifest
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +14,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //FusedLocationProviderClient(LocationManger대신 많이씀)
+        requestLocationPermissions()
+
         //msemon project commit test
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if(!locationPermissionGranted){
+            finish()
+
+        }else{
+            //fetchData
+
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                ACCESS_COARSE_LOCATION,
+                ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+    companion object{
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
 }
